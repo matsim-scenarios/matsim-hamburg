b0a2e4b752a97df5fd2ffe6ff6715b0dba1229b0
Mengzhuoxiao <michaelmeng2015@outlook.com>
2020-10-06 14:14:14 +0200

    delete hamburgSubtourModeChoice test

--- src/test/java/org/matsim/run/RunHamburgScenarioTest.java
+++ src/test/java/org/matsim/run/RunHamburgScenarioTest.java
@@ -39,39 +39,9 @@ public class RunHamburgScenarioTest {
         controler.run();
     }
 
+
     @Test
     public void u17CarUsingTest(){
-
-        String args[] = new String[]{
-                "test/input/test-hamburg.config.xml" ,
-                "--config:controler.lastIteration" , "3"
-        };
-
-        Config config = prepareConfig(args);
-
-        config.controler().setRunId("u17CarUsingTest");
-        config.controler().setOutputDirectory(utils.getOutputDirectory());
-
-        config.plans().setInputFile("test-u17-hamburg.plans.xml");
-        config.subtourModeChoice().setConsiderCarAvailability(true);
-
-        config.strategy().getStrategySettings().forEach(strategySettings ->
-            {if(strategySettings.getStrategyName().equals("SubtourModeChoice"))
-                strategySettings.setWeight(100.);
-            });
-
-        Scenario scenario = prepareScenario(config);
-        Controler controler = prepareControler(scenario);
-
-        controler.run();
-
-        ModeStatsControlerListener modeStatsControlerListener = controler.getInjector().getInstance(ModeStatsControlerListener.class);
-
-        Assert.assertFalse("U17 person should not drive car",modeStatsControlerListener.getModeHistories().containsKey("car"));
-    }
-
-    @Test
-    public void HamburgSubtourModeChoiceTest(){
         String args[] = new String[]{
                 "test/input/test-hamburg.config.xml" ,
                 "--config:controler.lastIteration" , "10"
@@ -79,7 +49,7 @@ public class RunHamburgScenarioTest {
 
         Config config = prepareConfig(args);
 
-        config.controler().setRunId("HamburgSubtourModeChoiceTest");
+        config.controler().setRunId("u17CarUsingTest");
         config.controler().setOutputDirectory(utils.getOutputDirectory());
 
         config.plans().setInputFile("test-u17-hamburg.plans.xml");
