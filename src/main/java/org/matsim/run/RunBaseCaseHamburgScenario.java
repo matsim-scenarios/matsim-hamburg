d0d55bdf387f069444009ab25aaf9c56afb4880e
ikaddoura <kaddoura@vsp.tu-berlin.de>
2021-03-17 11:08:45 +0200

    new kinematic waves approach settings as comment

--- src/main/java/org/matsim/run/RunBaseCaseHamburgScenario.java
+++ src/main/java/org/matsim/run/RunBaseCaseHamburgScenario.java
@@ -24,6 +24,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup.TrafficDynamics;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.mobsim.qsim.AbstractQSimModule;
@@ -264,6 +265,10 @@ public class RunBaseCaseHamburgScenario {
             config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("shop_other_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(8. * 3600.).setClosingTime(20. * 3600.));
             config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("educ_kiga_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(8. * 3600.).setClosingTime(18. * 3600.));
         }
+        
+//        config.qsim().setTrafficDynamics(TrafficDynamics.kinematicWaves);
+//        config.qsim().setTrafficDynamicsCorrectionApproach(TrafficDynamicsCorrectionApproach.INCREASE_NUMBER_OF_LANES);
+        
         return config;
     }
 }
