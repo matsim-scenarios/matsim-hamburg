7ca45a2b90af326ddb5eeb8732cf537aa27b1cee
GregorRyb <gregor.rybczak@alice.de>
2021-06-11 13:47:15 +0200

    Changing MobBug run class to make more flexible

--- src/main/java/org/matsim/run/RunBaseCaseWithMobilityBudgetV2.java
+++ src/main/java/org/matsim/run/RunBaseCaseWithMobilityBudgetV2.java
@@ -53,6 +53,7 @@ public class RunBaseCaseWithMobilityBudgetV2 {
     public static final double[] Y_EXTENT = new double[]{5866434.167201331, 5996884.970634732};
     public static final HashMap<Id<Person>, Double > personsWithMobilityBudget = new HashMap<>();
     public static double totalSumMobilityBudget = 0;
+    static double dailyMobilityBudget;
 
     public static void main(String[] args) throws ParseException, IOException {
 
@@ -64,54 +65,22 @@ public class RunBaseCaseWithMobilityBudgetV2 {
             args = new String[] {"scenarios/input/hamburg-v1.1-10pct.config.xml"};
         }
 
-        RunBaseCaseHamburgScenario baseCaseHH = new RunBaseCaseHamburgScenario();
-        baseCaseHH.run(args);
+        RunBaseCaseWithMobilityBudgetV2.run(args);
     }
 
-    private void run(String[] args) throws IOException {
+    private static void run(String[] args) throws IOException {
 
         Config config = prepareConfig(args);
         Scenario scenario = prepareScenario(config);
-
         Controler controler = prepareControler(scenario);
-
         controler.run();
         log.info("Done.");
     }
 
 
     public static Controler prepareControler(Scenario scenario) {
-        Controler controler = new Controler(scenario);
 
-        // use the sbb pt raptor router
-        controler.addOverridingModule( new AbstractModule() {
-            @Override
-            public void install() {
-                install( new SwissRailRaptorModule() );
-            }
-        } );
-
-        // use PersonIncomeSpecificScoringFunction if is needed
-        controler.addOverridingModule(new AbstractModule() {
-            @Override
-            public void install() {
-                if(ConfigUtils.addOrGetModule(scenario.getConfig(), HamburgExperimentalConfigGroup.class).isUsePersonIncomeBasedScoring()){
-                    this.bindScoringFunctionFactory().to(IncomeDependentPlanScoringFunctionFactory.class);
-                }
-            }
-        });
-        // use HereApiValidator if is needed
-        controler.addOverridingModule(new AbstractModule() {
-            @Override
-            public void install() {
-                if(ConfigUtils.addOrGetModule(scenario.getConfig(),HereAPITravelTimeValidationConfigGroup.class).isUseHereAPI()){
-                    CarTripsExtractor carTripsExtractor = new CarTripsExtractor(scenario.getPopulation().getPersons().keySet(), scenario.getNetwork());
-                    this.addEventHandlerBinding().toInstance(carTripsExtractor);
-                    this.addControlerListenerBinding().to(HereAPIControlerListener.class);
-                    this.bind(HereAPITravelTimeValidation.class).toInstance(new HereAPITravelTimeValidation(carTripsExtractor,scenario.getConfig()));
-                }
-            }
-        });
+        Controler controler = RunBaseCaseHamburgScenario.prepareControler(scenario);
 
         MobilityBudgetEventHandler mobilityBudgetEventHandler = new MobilityBudgetEventHandler();
         controler.addOverridingModule(new AbstractModule() {
@@ -120,30 +89,6 @@ public class RunBaseCaseWithMobilityBudgetV2 {
                 addEventHandlerBinding().toInstance(mobilityBudgetEventHandler);
             }
         });
-
-
-        controler.addOverridingModule(new AbstractModule() {
-            @Override
-            public void install() {
-                this.bind(PlanBasedTripsFileWriter.class).asEagerSingleton();
-                this.addControlerListenerBinding().to(PlanBasedTripsWriterControlerListener.class);
-            }
-        });
-
-        // use link-based park pressure
-        if(ConfigUtils.addOrGetModule(controler.getConfig(),HamburgExperimentalConfigGroup.class).isUseLinkBasedParkPressure()){
-
-            controler.addOverridingModule(new AbstractModule() {
-
-                @Override
-                public void install() {
-                    this.addEventHandlerBinding().to(UtilityBasedParkingPressureEventHandler.class);
-                }
-            });
-        }
-
-        // add Freight
-        AdjustScenarioForFreight.adjustControlerForFreight(controler, AdjustScenarioForFreight.getFreightModes());
 
         controler.addControlerListener(new AfterMobsimListener() {
             @Override
@@ -158,6 +103,7 @@ public class RunBaseCaseWithMobilityBudgetV2 {
             }
 
         });
+
         log.info("This iteration the totalSumMobilityBudget paid to the Agents was:" + totalSumMobilityBudget);
 
         return controler;
@@ -165,73 +111,11 @@ public class RunBaseCaseWithMobilityBudgetV2 {
 
     public static Scenario prepareScenario(Config config) throws IOException {
 
-        /*
-         * We need to set the DrtRouteFactory before loading the scenario. Otherwise DrtRoutes in input plans are loaded
-         * as GenericRouteImpls and will later cause exceptions in DrtRequestCreator. So we do this here, although this
-         * class is also used for runs without drt.
-         */
-        final Scenario scenario = ScenarioUtils.createScenario( config );
+        Scenario scenario = RunBaseCaseHamburgScenario.prepareScenario(config);
+        //ScenarioUtils.loadScenario(scenario);
 
-        RouteFactories routeFactories = scenario.getPopulation().getFactory().getRouteFactories();
-        routeFactories.setRouteFactory(DrtRoute.class, new DrtRouteFactory());
-
-        ScenarioUtils.loadScenario(scenario);
-
-        HamburgExperimentalConfigGroup hamburgExperimentalConfigGroup = ConfigUtils.addOrGetModule(config, HamburgExperimentalConfigGroup.class);
-
-        org.matsim.core.population.PopulationUtils.sampleDown(scenario.getPopulation(), hamburgExperimentalConfigGroup.getPopulationDownsampleFactor());
-
-        for (Person person :
-                scenario.getPopulation().getPersons().values()) {
-            Plan selectedPlan = person.getSelectedPlan();
-            person.getPlans().clear();
-            person.addPlan(selectedPlan);
-            person.setSelectedPlan(selectedPlan);
-        }
-
-        // increase flowspeed for links, where flowspeed lower than 50kmh
-        for (Link link : scenario.getNetwork().getLinks().values()) {
-            if (link.getFreespeed() < 25.5 / 3.6) {
-                link.setFreespeed(link.getFreespeed() * hamburgExperimentalConfigGroup.getFreeSpeedFactor());
-            }
-        }
-
-        // add parkPressureAttribute
-        if(hamburgExperimentalConfigGroup.isUseLinkBasedParkPressure()){
-            if (hamburgExperimentalConfigGroup.getParkPressureLinkAttributeFile() != null) {
-                log.info("Adding missing park pressure link attributes based on provided files...");
-                NetworkParkPressureReader networkParkPressureReader = new NetworkParkPressureReader(scenario.getNetwork(),hamburgExperimentalConfigGroup);
-                networkParkPressureReader.addLinkParkTimeAsAttribute();
-                log.info("Adding missing park pressure link attributes based on provided files... Done.");
-            }
-        }
-
-
-        // add Freight traffic
-        if(hamburgExperimentalConfigGroup.isFilterCommercial()){
-            List<Id<Person>> personIds = new LinkedList<>();
-            for(Person person : scenario.getPopulation().getPersons().values()) {
-                if (person.getId().toString().contains("commercial")) {
-                    personIds.add(person.getId());
-                }
-            }
-            for (Id<Person> personId: personIds) {
-                scenario.getPopulation().removePerson(personId);
-            }
-        } else
-            AdjustScenarioForFreight.adjustScenarioForFreight(scenario, AdjustScenarioForFreight.getFreightModes());
-
-
-
-        if (hamburgExperimentalConfigGroup.isIncreaseStorageCapacity()) {
-            for (Link link: scenario.getNetwork().getLinks().values()) {
-                double originalStorageCapacity = link.getLength() / 15 * link.getNumberOfLanes() * hamburgExperimentalConfigGroup.getSampleSize()
-                        / 100.0;
-                int minimumLaneRequred = (int) (1 / originalStorageCapacity + 1);
-                if (originalStorageCapacity < 1) {
-                    link.setNumberOfLanes(minimumLaneRequred);
-                }
-            }
+        for (Person person : scenario.getPopulation().getPersons().values()) {
+            personsWithMobilityBudget.put(person.getId(), dailyMobilityBudget);
         }
 
         return scenario;
@@ -239,41 +123,9 @@ public class RunBaseCaseWithMobilityBudgetV2 {
 
     public static Config prepareConfig(String[] args, ConfigGroup... customModules) {
 
-        ConfigGroup[] customModulesAll = new ConfigGroup[customModules.length];
-
-        int counter = 0;
-        for (ConfigGroup customModule : customModules) {
-            customModulesAll[counter] = customModule;
-            counter++;
-        }
-
-
-        final Config config = ConfigUtils.loadConfig(args[0], customModulesAll);
-        ConfigUtils.addOrGetModule(config, HamburgExperimentalConfigGroup.class);
-        ConfigUtils.addOrGetModule(config, HereAPITravelTimeValidationConfigGroup.class);
-
-
-        // delete default modes
-        config.plansCalcRoute().removeModeRoutingParams(TransportMode.ride);
-
-        String[] typedArgs = Arrays.copyOfRange(args, 1, args.length);
-        ConfigUtils.applyCommandline(config, typedArgs);
-
-        //todo: think about opening and closing time, there can be some overnight activities like shopping or business...
-        for (long ii = 600; ii <= 97200; ii += 600) {
-
-            for (String act : List.of("educ_higher", "educ_tertiary", "educ_other", "home", "educ_primary", "errands", "educ_secondary", "visit", "other")) {
-                config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams(act + "_" + ii + ".0").setTypicalDuration(ii));
-            }
-
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("work_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(6. * 3600.).setClosingTime(20. * 3600.));
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("business_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(6. * 3600.).setClosingTime(20. * 3600.));
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("leisure_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(9. * 3600.).setClosingTime(27. * 3600.));
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("shop_daily_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(8. * 3600.).setClosingTime(20. * 3600.));
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("shop_other_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(8. * 3600.).setClosingTime(20. * 3600.));
-            config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("educ_kiga_" + ii + ".0").setTypicalDuration(ii).setOpeningTime(8. * 3600.).setClosingTime(18. * 3600.));
-        }
-
+        Config config = RunBaseCaseHamburgScenario.prepareConfig(args, customModules);
+        dailyMobilityBudget = Double.parseDouble(args[6]);
+        log.info(dailyMobilityBudget);
         return config;
     }
 }
