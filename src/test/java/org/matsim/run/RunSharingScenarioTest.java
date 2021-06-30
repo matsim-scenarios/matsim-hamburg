4cab8f0532d6d73067935caf6949419c417d8cd6
tschlenther <schlenther@vsp.tu-berlin.de>
2021-06-30 14:56:33 +0200

    run sharing tests

--- src/test/java/org/matsim/run/RunSharingScenarioTest.java
+++ src/test/java/org/matsim/run/RunSharingScenarioTest.java
@@ -7,15 +7,7 @@ import org.matsim.contrib.sharing.run.SharingConfigGroup;
 import org.matsim.contrib.sharing.run.SharingServiceConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.StrategyConfigGroup;
-import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.router.AnalysisMainModeIdentifier;
-import org.matsim.core.router.RoutingModeMainModeIdentifier;
-import org.matsim.sharingFare.SharingFareHandler;
-import org.matsim.sharingFare.SharingFaresConfigGroup;
-import org.matsim.sharingFare.SharingServiceFaresConfigGroup;
-import org.matsim.sharingFare.TeleportedSharingFareHandler;
 import org.matsim.testcases.MatsimTestUtils;
 
 import java.io.IOException;
@@ -32,7 +24,7 @@ public class RunSharingScenarioTest {
     @Test
     public void runTest() throws IOException {
 
-        String args[] = new String[]{
+        String[] args = new String[]{
                 "scenarios/input/sharing/hamburg-v2.0-10pct-sharing.config.xml" ,
                 "--config:controler.lastIteration" , "0",
                 "--config:hamburgExperimental.freeSpeedFactor", "1.2",
@@ -41,11 +33,10 @@ public class RunSharingScenarioTest {
                 "--config:hamburgExperimental.useLinkBasedParkPressure","true",
                 "--config:hamburgExperimental.parkPressureScoreConstant","-2.",
                 "--config:network.inputNetworkFile" , "../../../test/input/test-hamburg-with-pt-network.xml.gz",
-                "--config:plans.inputPlansFile" , "test/input/plans/test-scar-sharing-user.plans.xml",
+                "--config:plans.inputPlansFile" , "../../../test/input/plans/test-scar-sharing-user.plans.xml",
         };
 
         Config config = prepareConfig(args);
-
 
         config.transit().setUseTransit(false);
 
@@ -53,7 +44,6 @@ public class RunSharingScenarioTest {
         config.controler().setWriteEventsInterval(1);
         config.controler().setWritePlansInterval(1);
         config.controler().setOutputDirectory(utils.getOutputDirectory());
-
 
         SharingConfigGroup sharingConfigGroup = ConfigUtils.addOrGetModule(config,SharingConfigGroup.class);
         for (SharingServiceConfigGroup service : sharingConfigGroup.getServices()) {
@@ -73,9 +63,7 @@ public class RunSharingScenarioTest {
         Scenario scenario = prepareScenario(config);
         Controler controler = prepareControler(scenario);
 
-
         controler.run();
-
-
     }
+
 }
