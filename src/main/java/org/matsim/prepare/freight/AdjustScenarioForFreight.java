b2b76067757f535a33de0396fa1c36c1c7ebe53f
tschlenther <schlenther@vsp.tu-berlin.de>
2021-09-08 16:55:39 +0200

    delete coords from commercial activities: commercial agents do not need access and egress walks

--- src/main/java/org/matsim/prepare/freight/AdjustScenarioForFreight.java
+++ src/main/java/org/matsim/prepare/freight/AdjustScenarioForFreight.java
@@ -4,11 +4,14 @@ import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 
 import java.util.Arrays;
@@ -51,6 +54,20 @@ public class AdjustScenarioForFreight {
         for (String mode : modes) {
             config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams(mode).setMonetaryDistanceRate(-0.0004));
         }
+
+        log.info("will delete routes and coords from commercial activities!! This requires that the input plans were routed at least once, beforehand!");
+        scenario.getPopulation().getPersons().values().stream()
+                .filter(person -> PopulationUtils.getSubpopulation(person).equals(COMMERCIAL))
+                .flatMap(person -> person.getSelectedPlan().getPlanElements().stream())
+                .forEach(planElement -> {
+                    if(planElement instanceof Activity){
+                        ((Activity) planElement).setCoord(null);
+                    } else {
+                        ((Leg) planElement).setRoute(null);
+                    }
+                });
+
+        log.info("finished preparing scenario for freight...");
     }
 
     private static HashSet<String> getModesWithFreight(List<String> modes, Collection<String> allowedModes) {
