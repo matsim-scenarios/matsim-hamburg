a850d75403b9282f923fdc84593ea2db423f49bd
tschlenther <schlenther@vsp.tu-berlin.de>
2021-09-09 22:41:18 +0200

    analyze population in terms of responsiveness to SubtourModeChoice

---
+++ src/main/java/org/matsim/analysis/AnalysePlansForSubtourModeChoice.java
@@ -0,0 +1,184 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.run.HamburgAnalysisMainModeIdentifier;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AnalysePlansForSubtourModeChoice {
+
+	private static final HamburgAnalysisMainModeIdentifier mainModeIdentifier = new HamburgAnalysisMainModeIdentifier();
+	private static final Set<String> chainBasedModes = Set.of(TransportMode.bike, TransportMode.car);
+	private static final Set<String> modes = Set.of("car","pt","bike","walk");
+
+
+	public static void main(String[] args) {
+
+		Population population = PopulationUtils.readPopulation("../../svn/shared-svn/projects/matsim-hamburg/hamburg-v2/hamburg-v2.0/input/hamburg-v2.0-10pct.plans.xml.gz");
+
+		List<Id<Person>> nonCarUsers = population.getPersons().values().stream()
+				.filter(person -> !isUser(person, TransportMode.car))
+				.map(person -> person.getId())
+				.collect(Collectors.toList());
+
+		nonCarUsers.forEach(nonUser -> population.removePerson(nonUser));
+
+		Map<Integer, Integer> nrOfSubtours2NrOfAgents = new HashMap<>();
+		Map<Integer, Integer> nrOfProblematicSubtours2NrOfAgents = new HashMap<>();
+
+		int nrOfAgentsWithFixedPlans = 0;
+		int nrOfAgentsWith0Trips = 0;
+
+		for(Person person :population.getPersons().values()){
+
+			Collection<TripStructureUtils.Subtour> subtours = TripStructureUtils.getSubtours(person.getSelectedPlan());
+
+			if(TripStructureUtils.getTrips(person.getSelectedPlan()).size() == 0) nrOfAgentsWith0Trips++;
+
+			nrOfSubtours2NrOfAgents.compute(subtours.size(), (k,v) -> v == null ? 1 : v + 1);
+
+			int nrOfProblematicSubTours = (int) subtours.stream()
+					.filter(AnalysePlansForSubtourModeChoice::isProblematic)
+					.count();
+
+			if (nrOfProblematicSubTours == subtours.size()){
+				nrOfAgentsWithFixedPlans ++;
+			}
+			nrOfProblematicSubtours2NrOfAgents.compute(nrOfProblematicSubTours, (k,v) -> v == null ? 1 : v + 1);
+
+		}
+
+		System.out.println("###################");
+		System.out.println("### total number of agents: " + population.getPersons().size());
+		System.out.println("###################");
+		System.out.println("### number of agents with 0 trips: " + nrOfAgentsWith0Trips);
+		System.out.println("###################");
+		nrOfSubtours2NrOfAgents.forEach( (k,v) -> System.out.println("### " + k + " subtours:\t" + v + " agents"));
+		System.out.println("###################");
+		nrOfProblematicSubtours2NrOfAgents.forEach( (k,v) -> System.out.println("### " + k + " problematic subtours:\t" + v + " agents"));
+		System.out.println("###################");
+		System.out.println("### number of agents with completely problematic plans: " + nrOfAgentsWithFixedPlans + " = " + ((double) nrOfAgentsWithFixedPlans / ((double) population.getPersons().size()) + "%"));
+		System.out.println("###################");
+		System.out.println("######FINISHED#####");
+	}
+
+	private static boolean isUser(Person person, String mode) {
+		return TripStructureUtils.getLegs(person.getSelectedPlan()).stream()
+				.filter(leg -> leg.getMode().equals(mode))
+				.findAny()
+				.isPresent();
+	}
+
+
+	/**
+	 * in other words: will not be mutated by ChoosRandomeLegModeForSubtour
+	 * @param subtour
+	 * @return
+	 */
+	private static boolean isProblematic(TripStructureUtils.Subtour subtour) {
+		return !subtour.isClosed() || !isMassConserving(subtour) || containsUnknownMode(subtour);
+	}
+
+
+	///-----------------------copied from ChooseRandomLegModeForSubtour
+
+	private static boolean containsUnknownMode(final TripStructureUtils.Subtour subtour) {
+		for (TripStructureUtils.Trip trip : subtour.getTrips()) {
+			if (!modes.contains( mainModeIdentifier.identifyMainMode( trip.getTripElements() ))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean isMassConserving(final TripStructureUtils.Subtour subtour) {
+		for (String mode : chainBasedModes) {
+			if (!isMassConserving(subtour, mode)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isMassConserving(
+			final TripStructureUtils.Subtour subtour,
+			final String mode) {
+		final Activity firstOrigin =
+				findFirstOriginOfMode(
+						subtour.getTrips(),
+						mode);
+
+		if (firstOrigin == null) {
+			return true;
+		}
+
+		final Activity lastDestination =
+				findLastDestinationOfMode(
+						subtour.getTrips(),
+						mode);
+
+		return atSameLocation(firstOrigin, lastDestination);
+	}
+
+
+	private static boolean atSameLocation(Activity firstLegUsingMode,
+								   Activity lastLegUsingMode) {
+		return firstLegUsingMode.getFacilityId()!=null ?
+				firstLegUsingMode.getFacilityId().equals(
+						lastLegUsingMode.getFacilityId() ) :
+				firstLegUsingMode.getLinkId().equals(
+						lastLegUsingMode.getLinkId() );
+	}
+
+	private static Activity findLastDestinationOfMode(
+			final List<TripStructureUtils.Trip> tripsToSearch,
+			final String mode) {
+		final List<TripStructureUtils.Trip> reversed = new ArrayList<>(tripsToSearch);
+		Collections.reverse( reversed );
+		for (TripStructureUtils.Trip trip : reversed) {
+			if ( mode.equals( mainModeIdentifier.identifyMainMode( trip.getTripElements() ) ) ) {
+				return trip.getDestinationActivity();
+			}
+		}
+		return null;
+	}
+
+	private static Activity findFirstOriginOfMode(
+			final List<TripStructureUtils.Trip> tripsToSearch,
+			final String mode) {
+		for (TripStructureUtils.Trip trip : tripsToSearch) {
+			if ( mode.equals( mainModeIdentifier.identifyMainMode( trip.getTripElements() ) ) ) {
+				return trip.getOriginActivity();
+			}
+		}
+		return null;
+	}
+
+}
