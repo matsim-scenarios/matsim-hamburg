73239b3a0fbb00eae3c16d8b1bd0575e0eed4cc7
tschlenther <schlenther@vsp.tu-berlin.de>
2021-09-27 19:21:47 +0200

    HamburgAccidentsNetworkModification

--- src/main/java/org/matsim/analysis/accidents/HamburgAccidentsNetworkModification.java
+++ src/main/java/org/matsim/analysis/accidents/HamburgAccidentsNetworkModification.java
@@ -1,12 +1,5 @@
 package org.matsim.analysis.accidents;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
 import org.geotools.data.DataStore;
 import org.geotools.data.FileDataStoreFinder;
@@ -16,16 +9,28 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.accidents.AccidentsConfigGroup;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.gis.ShapeFileReader;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.misc.Counter;
+import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 import org.opengis.feature.simple.SimpleFeature;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 /**
  * @author mmayobre, ikaddoura
@@ -36,11 +41,134 @@ public class HamburgAccidentsNetworkModification {
 
     private final Scenario scenario;
 
-    public HamburgAccidentsNetworkModification(Scenario scenario) {
+    @Deprecated //use static method
+    HamburgAccidentsNetworkModification(Scenario scenario) {
         this.scenario = scenario;
     }
 
-    public Network setLinkAttributsBasedOnOSMFile(String landuseOsmFile, String osmCRS, String[] tunnelLinkIDs, String[] planfreeLinkIDs) throws MalformedURLException, IOException {
+    public static void main(String[] args) {
+
+        AccidentsConfigGroup accidentsSettings = new AccidentsConfigGroup();
+        Network network = NetworkUtils.readNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg-v2.0-network-with-pt.xml.gz");
+
+        Set<Id<Link>> tunnelLinks = readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_tunnel_2021.csv");
+        Set<Id<Link>> planfreeLinks = readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_planfree_2021.csv");
+
+        HamburgAccidentsNetworkModification.setLinkAttributesBasedOnInTownShapeFile(accidentsSettings,
+                network,
+                "D:/svn/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/shp/innerorts-ausserorts/hamburg_hvv_innerorts_inkl_HH_city_reduced.shp",
+                tunnelLinks,
+                planfreeLinks);
+
+        NetworkUtils.writeNetwork( network,"D:/svn/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg-v2.0-network-with-pt-with-accidentAttributes.xml.gz");
+
+    }
+
+    static void setLinkAttributesBasedOnInTownShapeFile(AccidentsConfigGroup accidentsConfigGroup, Network network, String pathToInTownShapeFile, Set<Id<Link>> tunnelLinkIds, Set<Id<Link>> planfreeLinkIds){
+        log.info("Initializing all link-specific information...");
+
+        List<PreparedGeometry> inTownShapes = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(pathToInTownShapeFile));
+
+        Counter counter = new Counter("link # ");
+        for (Link link : network.getLinks().values()) {
+            counter.incCounter();
+
+            link.getAttributes().putAttribute(accidentsConfigGroup.getAccidentsComputationMethodAttributeName(), AccidentsConfigGroup.AccidentsComputationMethod.BVWP.toString());
+
+            // 'plangleich', 'planfrei' or tunnel?
+            int junctionType = 1; //default plangleich
+            // builtup or not builtup area? and Kraftfahrstrasse or not Kraftfahrstrasse?
+            int areaAndRoadType = 0; // default = outside of town/not built-up area and no kfz straße
+            //number of lanes
+            int lanes;
+
+            Set<String> modes = link.getAllowedModes();
+            if(modes.contains(TransportMode.pt) && !modes.contains(TransportMode.car)){
+                //IMPORTANT: we ignore accident costs of pure pt links. So far, it is unclear whether the link is a street or a railway, for which the cost rates differ by a factor of 10!
+                //so far, pt links have not been handled separately at all! As we have varying pt schedules with varying model areas in between the scenarios, i chose to ignore pt incidents cost for the first approach!
+                //for a more sophisticated version, one could look up the line type from the pt schedule and distinguish railway pt lines and bus lines.
+
+                junctionType = 0; //plan free
+                areaAndRoadType = 0; //not built-up area, no kfz-straße
+                lanes = 1;
+
+            } else {
+                //car is allowed. this link seems to model a street/roadway.
+
+                if(planfreeLinkIds.contains(link.getId())){
+                    junctionType = 0;
+                }
+                if(tunnelLinkIds.contains(link.getId())){ //if both plan free and tunnel then use tunnel
+                    junctionType = 2;
+                }
+
+                //consider middle of the link
+                if(ShpGeometryUtils.isCoordInPreparedGeometries(link.getCoord(), inTownShapes)){
+                    areaAndRoadType += 1;
+                }
+
+                //if more than 16 m/s then we model it as kraftfahrstrasse
+                if(link.getFreespeed() > 16.){
+                    areaAndRoadType += 2;
+                }
+
+                if (link.getNumberOfLanes() > 4.0){
+                    log.warn("Set number of lanes to 4");
+                    lanes = 4;
+                }
+                else if(link.getNumberOfLanes()<1.0) {
+                    lanes = 1;
+                }
+                else {
+                    lanes = (int) link.getNumberOfLanes();
+                }
+            }
+            link.getAttributes().putAttribute( AccidentsConfigGroup.BVWP_ROAD_TYPE_ATTRIBUTE_NAME, junctionType + "," + areaAndRoadType + "," + lanes);
+        }
+        log.info("Initializing all link-specific information... Done.");
+    }
+
+    static Set<Id<Link>> readCSVFile(String csvFile) {
+        Set<Id<Link>> links = new HashSet<>();
+
+        BufferedReader br = IOUtils.getBufferedReader(csvFile);
+
+        String line = null;
+        try {
+            line = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            int countWarning = 0;
+            while ((line = br.readLine()) != null) {
+
+                String[] columns = line.split(";");
+                Id<Link> linkId = null;
+                for (int column = 0; column < columns.length; column++) {
+                    if (column == 0) {
+                        linkId = Id.createLinkId(columns[column]);
+                    } else {
+                        if (countWarning < 1) {
+                            log.warn("Expecting the link Id to be in the first column. Ignoring further columns...");
+                        } else if (countWarning == 1) {
+                            log.warn("This message is only given once.");
+                        }
+                        countWarning++;
+                    }
+                }
+                log.info("Adding link ID " + linkId);
+                links.add(linkId);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return links;
+    }
+
+    @Deprecated //this was copied from elsewhere (contrib or matsim-berlin)
+    Network setLinkAttributesBasedOnOSMFile(String landuseOsmFile, String osmCRS, String[] tunnelLinkIDs, String[] planfreeLinkIDs) throws IOException {
 
         AccidentsConfigGroup accidentsCfg = (AccidentsConfigGroup) scenario.getConfig().getModules().get(AccidentsConfigGroup.GROUP_NAME);
 
@@ -159,6 +287,7 @@ public class HamburgAccidentsNetworkModification {
         return scenario.getNetwork();
     }
 
+    @Deprecated
     private String getOSMLandUseFeatureBBId(Link link, Map<String, SimpleFeature> landUseFeaturesBB, String osmCRS) {
 
         if (landUseFeaturesBB == null || landUseFeaturesBB.isEmpty()) return null;
@@ -265,6 +394,7 @@ public class HamburgAccidentsNetworkModification {
         return null;
     }
 
+    @Deprecated
     private Point transformPointFromGK4ToOSMCRS(Point pointGK4, String osmCRS) {
         CoordinateTransformation ctGK4toOSMCRS = TransformationFactory.getCoordinateTransformation("EPSG:31468", osmCRS);
 
--- src/main/java/org/matsim/analysis/accidents/RunAccidentsHamburg.java
+++ src/main/java/org/matsim/analysis/accidents/RunAccidentsHamburg.java
@@ -10,13 +10,10 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.utils.io.IOUtils;
 import org.matsim.run.RunBaseCaseHamburgScenario;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * @author ikaddoura, mmayobre, grybczak
@@ -34,7 +31,7 @@ public class RunAccidentsHamburg {
         main.run(args);
     }
 
-    private void run(String[] args) throws MalformedURLException, IOException {
+    private void run(String[] args) throws IOException {
 
         log.info("Loading scenario...");
         Config config = RunBaseCaseHamburgScenario.prepareConfig(args);
@@ -45,59 +42,19 @@ public class RunAccidentsHamburg {
         final Scenario scenario = RunBaseCaseHamburgScenario.prepareScenario(config);
 
         // Preprocess network
-        HamburgAccidentsNetworkModification hamburgAccidentsNetworkModification = new HamburgAccidentsNetworkModification(scenario);
-
-        String[] tunnelLinks = readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_tunnel_2021.csv");
-        String[] planfreeLinks = readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_planfree_2021.csv");
-
+        Set<Id<Link>> tunnelLinks = HamburgAccidentsNetworkModification.readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_tunnel_2021.csv");
+        Set<Id<Link>> planfreeLinks = HamburgAccidentsNetworkModification.readCSVFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/baseCase/input/hamburg_hvv_planfree_2021.csv");
+        HamburgAccidentsNetworkModification.setLinkAttributesBasedOnInTownShapeFile(accidentsSettings, scenario.getNetwork(), "", tunnelLinks, planfreeLinks);
 
         //networkModification.setLinkAttributsBasedOnOSMFile("osmlandUseFile", "EPSG:31468" , tunnelLinks, planfreeLinks );
-        hamburgAccidentsNetworkModification.setLinkAttributsBasedOnOSMFile(null, null, tunnelLinks, planfreeLinks);
+//        hamburgAccidentsNetworkModification.setLinkAttributesBasedOnOSMFile(null, null, tunnelLinks, planfreeLinks);
+
         Controler controler = RunBaseCaseHamburgScenario.prepareControler(scenario);
         controler.addOverridingModule(new AccidentsModule());
         controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
         controler.run();
     }
 
-    private String[] readCSVFile(String csvFile) {
-        ArrayList<Id<Link>> links = new ArrayList<>();
 
-        BufferedReader br = IOUtils.getBufferedReader(csvFile);
-
-        String line = null;
-        try {
-            line = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            int countWarning = 0;
-            while ((line = br.readLine()) != null) {
-
-                String[] columns = line.split(";");
-                Id<Link> linkId = null;
-                for (int column = 0; column < columns.length; column++) {
-                    if (column == 0) {
-                        linkId = Id.createLinkId(columns[column]);
-                    } else {
-                        if (countWarning < 1) {
-                            log.warn("Expecting the link Id to be in the first column. Ignoring further columns...");
-                        } else if (countWarning == 1) {
-                            log.warn("This message is only given once.");
-                        }
-                        countWarning++;
-                    }
-                }
-                log.info("Adding link ID " + linkId);
-                links.add(linkId);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String[] linkIDsArray = (String[]) links.toArray();
-        return linkIDsArray ;
-    }
 
 }
