94ffb5f311674592477672059d9e421bef51b79b
tschlenther <schlenther@vsp.tu-berlin.de>
2021-12-28 13:27:54 +0100

    write area type to home location file and make it a tsv instead of csv

--- src/main/java/org/matsim/analysis/GeneratePersonHomeLocation.java
+++ src/main/java/org/matsim/analysis/GeneratePersonHomeLocation.java
@@ -1,6 +1,8 @@
 package org.matsim.analysis;
 
 import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
@@ -10,34 +12,43 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.ShapeFileReader;
 import org.matsim.run.RunBaseCaseHamburgScenario;
+import org.opengis.feature.simple.SimpleFeature;
 
+import javax.annotation.Nullable;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author zmeng
  */
+//TODO: change to static methods
 public class GeneratePersonHomeLocation {
 
     private static final Logger log =Logger.getLogger(GeneratePersonHomeLocation.class);
     private Map<Person, Coord> person2homeLocation = new HashMap<>();
+    private Map<Coord, String> coord2Area = new HashMap<>();
 
     private Scenario scenario;
 
+    private static final String AREA_SHP_FILE = "../../svn/shared-svn/projects/realLabHH/data/hamburg_shapeFile/hamburg_merge/hamburg.shp";
 
     public static void main(String[] args) throws IOException {
 
-        String inputPlan = "../shared-svn/projects/matsim-hamburg/hamburg-v3/hamburg-v3.0-25pct.plans-not-calibrated.xml.gz";
-        String outputResult = "../shared-svn/projects/matsim-hamburg/hamburg-v3/hamburg-v3.0-person2HomeLocation.csv";
+        String inputPlan = "../../svn/shared-svn/projects/matsim-hamburg/hamburg-v3/hamburg-v3.0-25pct.plans-not-calibrated.xml.gz";
+        String outputResult = "../../svn/shared-svn/projects/matsim-hamburg/hamburg-v3/hamburg-v3.0-person2HomeLocation.tsv";
         GeneratePersonHomeLocation generatePersonHomeLocation = new GeneratePersonHomeLocation(inputPlan);
         generatePersonHomeLocation.generate();
-        generatePersonHomeLocation.write(outputResult,",");
+        generatePersonHomeLocation.write(outputResult,"\t");
 
     }
 
@@ -50,6 +61,9 @@ public class GeneratePersonHomeLocation {
     }
 
     private void generate() {
+        Collection<SimpleFeature> features = ShapeFileReader.getAllFeatures(AREA_SHP_FILE);
+
+
         for (Person person :
                 scenario.getPopulation().getPersons().values()) {
             Plan selectedPlan = person.getSelectedPlan();
@@ -58,6 +72,7 @@ public class GeneratePersonHomeLocation {
                     activities) {
                 if (act.getType().contains("home")){
                     this.person2homeLocation.put(person, act.getCoord());
+                    this.coord2Area.computeIfAbsent(act.getCoord(), coord -> getArea(coord, features));
                     break;
                 }
             }
@@ -67,17 +82,37 @@ public class GeneratePersonHomeLocation {
     private void write(String outputFile, String splitSymbol) throws IOException {
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8);
         BufferedWriter bw = new BufferedWriter(outputStreamWriter);
-        bw.write("person" + splitSymbol + "home_x" + splitSymbol + "home_y");
+        bw.write("person" + splitSymbol + "home_x" + splitSymbol + "home_y" + splitSymbol + "area");
 
         this.person2homeLocation.forEach((person, coord) -> {
             try {
                 bw.newLine();
-                bw.write(person.getId().toString() + splitSymbol + coord.getX() + splitSymbol + coord.getY());
+                bw.write(person.getId().toString() + splitSymbol + coord.getX() + splitSymbol + coord.getY() + splitSymbol + coord2Area.get(coord));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
         bw.close();
+    }
+
+    @Nullable
+    private String getArea(Coord coord, Collection<SimpleFeature> features) {
+        Point p = MGC.coord2Point(coord);
+        for (SimpleFeature feature : features) {
+            Geometry defaultGeometry = (Geometry) feature.getDefaultGeometry();
+            if (p.within(defaultGeometry)) {
+                Long areaType = (Long) feature.getAttribute("AreaType");
+                if (areaType == 0) {
+                    return "Metropolregion";
+                } else if (areaType == 1) {
+                    return "HVV_Umland";
+                } else if (areaType == 2) {
+                    return "Hamburg_city";
+                }
+                throw new IllegalArgumentException();
+            }
+        }
+        return "unknown";
     }
 }
