f2c16dea659c165115161c9895098b7cdaefb92a
tschlenther <schlenther@vsp.tu-berlin.de>
2022-12-27 14:44:04 +0100

    1: use a SNAPSHOT version of MATSim (from the informed-mode-choice branch) 2: adopt emission analysis script

--- src/main/java/org/matsim/analysis/emissions/RunOfflineAirPollutionAnalysisByEngineInformation.java
+++ src/main/java/org/matsim/analysis/emissions/RunOfflineAirPollutionAnalysisByEngineInformation.java
@@ -28,11 +28,11 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.emissions.EmissionModule;
+import org.matsim.contrib.emissions.EmissionUtils;
 import org.matsim.contrib.emissions.HbefaVehicleCategory;
 import org.matsim.contrib.emissions.Pollutant;
 import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
 import org.matsim.contrib.emissions.utils.EmissionsConfigGroup.DetailedVsAverageLookupBehavior;
-import org.matsim.contrib.emissions.utils.EmissionsConfigGroup.HbefaRoadTypeSource;
 import org.matsim.contrib.emissions.utils.EmissionsConfigGroup.NonScenarioVehicles;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
@@ -147,7 +147,7 @@ public class RunOfflineAirPollutionAnalysisByEngineInformation {
 		eConfig.setDetailedVsAverageLookupBehavior(DetailedVsAverageLookupBehavior.tryDetailedThenTechnologyAverageElseAbort);
 		eConfig.setDetailedColdEmissionFactorsFile(hbefaColdFile);
 		eConfig.setDetailedWarmEmissionFactorsFile(hbefaWarmFile);
-		eConfig.setHbefaRoadTypeSource(HbefaRoadTypeSource.fromLinkAttributes);
+//		eConfig.setHbefaRoadTypeSource(HbefaRoadTypeSource.fromLinkAttributes);
 		eConfig.setNonScenarioVehicles(NonScenarioVehicles.ignore);
 		eConfig.setWritingEmissionsEvents(true); //in the end, the EventXMLWriter is currently out commented (see below)
 
@@ -468,37 +468,37 @@ public class RunOfflineAirPollutionAnalysisByEngineInformation {
 			}
 
 			if(freespeed <= 8.333333333){ //30kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/Access/30");
+				link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Access/30");
 			} else if(freespeed <= 11.111111111){ //40kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/Access/40");
+				link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Access/40");
 			} else if(freespeed <= 13.888888889){ //50kmh
 				double lanes = link.getNumberOfLanes();
 				if(lanes <= 1.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Local/50");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Local/50");
 				} else if(lanes <= 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Distr/50");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Distr/50");
 				} else if(lanes > 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Trunk-City/50");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Trunk-City/50");
 				} else{
 					throw new RuntimeException("NoOfLanes not properly defined");
 				}
 			} else if(freespeed <= 16.666666667){ //60kmh
 				double lanes = link.getNumberOfLanes();
 				if(lanes <= 1.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Local/60");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Local/60");
 				} else if(lanes <= 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/Trunk-City/60");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/Trunk-City/60");
 				} else if(lanes > 2.0){
-					link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-City/60");
+					link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/MW-City/60");
 				} else{
 					throw new RuntimeException("NoOfLanes not properly defined");
 				}
 			} else if(freespeed <= 19.444444444){ //70kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-City/70");
+				link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/MW-City/70");
 			} else if(freespeed <= 22.222222222){ //80kmh
-				link.getAttributes().putAttribute("hbefa_road_type", "URB/MW-Nat./80");
+				link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "URB/MW-Nat./80");
 			} else if(freespeed > 22.222222222){ //faster
-				link.getAttributes().putAttribute("hbefa_road_type", "RUR/MW/>130");
+				link.getAttributes().putAttribute(EmissionUtils.HBEFA_ROAD_TYPE, "RUR/MW/>130");
 			} else{
 				throw new RuntimeException("Link not considered...");
 			}
